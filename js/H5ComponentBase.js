21ef994d047dea6e4da6e4f3abfa983f6221dfc2
huangyh <184620541@qq.com>
2017-08-10 16:31:04 +0800

    完成H5ComponentBase组件对象

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -3,15 +3,13 @@
 var H5Component = function(name,cfg){
     var cfg= cfg || {};
     var id=('h5_component'+Math.random()*100).replace('.','_');
-    var clsn='h5_component_'+cfg.type+' h5_component_name_'+name;
+    var clsn='h5_component_name_'+name+' h5_component_'+cfg.type;
     var component=$('<div class="h5_component '+clsn+'" id="'+id+'"></div>')
-    
-
     /*如果有cfg.text*/
     cfg.text   && component.text(cfg.text)
     cfg.width  && component.width(cfg.width/2)
     cfg.height && component.height(cfg.height/2)
-    // cfg.css && component.css(cfg.css)
+    cfg.css && component.css(cfg.css)
     cfg.bg && component.css('backgroundImage','url('+cfg.bg+')')
     if(cfg.center){
         component.css({
@@ -19,6 +17,17 @@ var H5Component = function(name,cfg){
             left:'50%'      
         })
     }
+    /*让基本图文组件绑定onLoad和onLeave事件*/
+    component.on('onLoad',function(){
+        $(this).addClass(clsn+' h5_component_Load').removeClass(' h5_component_Leave');
+        cfg.animateIn && $(this).animate(cfg.animateIn);
+        return false; //避免在page的调用触发中，陷入无限死循环
+    })
+    component.on('onLeave',function(){
+        $(this).addClass(clsn+' h5_component_Leave').removeClass(' h5_component_Load');
+        cfg.animateOut && $(this).animate(cfg.animateOut);
+        return false;
+    })
     return component;
     
 }
