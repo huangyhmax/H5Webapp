8563f1b744a0d0f47bb3bec6616259fb44a05a23
huangyh <184620541@qq.com>
2017-08-11 09:52:00 +0800

    完成柱状图的全部动画效果

---
+++ js/H5ComponentPolyline.js
@@ -0,0 +1,10 @@
+/*柱图组件对象*/
+/*
+所有的图表组件都是基于“基本的图文组件H5ComponentBase类”来实现的
+*/
+var H5ComponentPolyline = function(name,cfg){
+    var component = new H5Component( name,cfg);
+    
+
+    return component;
+}
