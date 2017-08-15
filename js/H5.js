f1e429c0aa5d76bda532dded840bc648637745d4
huangyh <184620541@qq.com>
2017-08-16 01:39:54 +0800

    注释分页的小点显示

--- js/H5.js
+++ js/H5.js
@@ -86,9 +86,9 @@ var H5=function(){
     this.loader = function(firstPage){
         /*在此完成fullpage*/
         this.element.fullpage({
-            navigation: true,
-            navigationPosition: 'right',
-            navigationTooltips: ['1', '2', '3'],
+            // navigation: true,
+            // navigationPosition: 'right',
+            // navigationTooltips: ['1', '2', '3'],
             onLeave: function(index, nextIndex, direction){
                 // debugger
                 $(this).find('.h5_component').trigger('onLeave')
